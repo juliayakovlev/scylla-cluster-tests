9a64766211c78dbf9e805dcae7279d0da3bba0db
Julia Yakovlev <julia.yakovlev@scylladb.com>
2024-01-25 15:32:40 +0200

    try to reproduce issue enterprise#3813

--- sdcm/sct_events/loaders.py
+++ sdcm/sct_events/loaders.py
@@ -245,10 +245,10 @@ class ScyllaBenchLogEvent(LogEvent, abstract=True):
 
 ScyllaBenchLogEvent.add_subevent_type("ConsistencyError", severity=Severity.ERROR, regex=r"received only")
 # Scylla-bench data validation was added by https://github.com/scylladb/scylla-bench/commit/3eb53d8ce11e5ad26062bcc662edb31dda521ccf
-ScyllaBenchLogEvent.add_subevent_type("DataValidationError", severity=Severity.CRITICAL,
+ScyllaBenchLogEvent.add_subevent_type("DataValidationError", severity=Severity.ERROR,
                                       regex=r"doesn't match |failed to validate data|failed to verify checksum|corrupt checksum or data|"
                                             r"data corruption")
-ScyllaBenchLogEvent.add_subevent_type("ParseDistributionError", severity=Severity.CRITICAL,
+ScyllaBenchLogEvent.add_subevent_type("ParseDistributionError", severity=Severity.ERROR,
                                       regex=r"missing parameter|unexpected parameter|unsupported|invalid")
 
 
